fd2ffe80650c462aab8d31f49978be37d0b4b15b
Ryosuke Miyamoto <yahsan2@gmail.com>
2018-12-18 22:52:33 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+#puppeteer-example-on-serverless-lambda
+
+## Check or Setup aws configure 
+https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html
+
+## Setup
+``` bash
+# clone repository
+$ git clone git@github.com:yahsan2/puppeteer-example-on-serverless-lambda.git 
+$ cd puppeteer-example-on-serverless-lambda.git 
+
+# install dependencies
+$ npm install # Or yarn install
+```
+
+## Scripts Command
+``` bash
+
+# deploy all functions to aws lambda 
+$ npm run deploy-all
+
+# invoke a specific function on aws lambda 
+$ npm run invoke -- puppeteer
+
+# invoke a specific function on local
+$ npm run local -- puppeteer
+
+# serverless info
+$ npm run info
+
+```
---
+++ index.js
@@ -0,0 +1,89 @@
+const launchChrome = require('@serverless-chrome/lambda');
+const CDP = require('chrome-remote-interface');
+const puppeteer = require('puppeteer');
+
+exports.handler = async (event, context, callback) => {
+    const {
+        url = 'https://poiit.me',
+        mobile = false,
+      } = event.queryStringParameters || {}
+    
+    let slsChrome = null;
+    let browser = null;
+    let page = null;
+
+    try {
+        slsChrome = await launchChrome({
+            flags: [
+                '--headless',
+                '--disable-gpu',
+                '--no-sandbox',
+                '--window-size=1048,743',
+                '--hide-scrollbars',
+                '--enable-logging',
+                '--ignore-certificate-errors',
+                '--disable-setuid-sandbox',
+            ]
+        });
+        
+        browser = await puppeteer.connect({ 
+            ignoreHTTPSErrors: true,
+            browserWSEndpoint: (await CDP.Version()).webSocketDebuggerUrl 
+        });
+
+        page = await browser.newPage();
+
+        page.setViewport({
+            width: 1048,
+            height: 743
+        })
+        
+        await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36')
+
+        await page.goto(url)
+        
+        await page.evaluate(() => {
+            var style = document.createElement('style')
+            style.textContent = `
+                @import url('//fonts.googleapis.com/css?family=M+PLUS+Rounded+1c|Roboto:300,400,500,700|Material+Icons');
+                div, input, a, p{ font-family: "M PLUS Rounded 1c", sans-serif; };`
+            document.head.appendChild(style);
+            document.body.style.fontFamily = "'M PLUS Rounded 1c', sans-serif";     
+        })
+        
+        await page.waitFor(2000);
+
+        const screenshotBuffer = await page.screenshot({
+            type: 'jpeg',
+            quality: 100,
+            encoding: 'binary'
+        });
+
+        return callback(null, {
+            statusCode: 200, 
+            isBase64Encoded: true,
+            headers: {
+                "Content-Type": "image/jpeg",
+            },
+            body: screenshotBuffer.toString('base64')
+        })
+    } catch (err) {
+        console.error(err);
+        return callback(null, {
+            statusCode: 500, 
+            body: err
+        });
+    } finally {        
+        if (page) {
+            await page.close();
+        }
+
+        if (browser) {
+            await browser.disconnect();
+        }
+
+        if (slsChrome) { 
+            await slsChrome.kill();
+        }
+    }
+};
